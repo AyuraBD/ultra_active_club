0b3484b0eca6e93dfc8c472b4e38c47629671669
Mohammad Ayub <5043ayub@gmail.com>
2022-09-29 19:31:15 +0600

    first question added

---
+++ src/components/Questions/Questions.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Questions.css'
+
+const Questions = () => {
+    return (
+        <div className="questions-container">
+            <div className='questions'>
+                <h2>Answers of Questions</h2>
+                <div className="first">
+                    <h3>01.How does react works?</h3>
+                    <p>React is a declarative, efficient, and flexible JavaScript library for building user interfaces. 'V' denotes the view in MVC. ReactJS is an open-source, component-based front end library responsible only for the view layer of the application. It is maintained by Facebook.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Questions;
